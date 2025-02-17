317628cd83815f0a30f14f6e12ae63490e4c8997
Muzammil1234-wolf <140607238+Muzammil1234-wolf@users.noreply.github.com>
2025-02-17 20:03:03 +0500

    Update Additionoperationonmatrices.cpp

--- Additionoperationonmatrices.cpp
+++ Additionoperationonmatrices.cpp
@@ -20,3 +20,10 @@ void addMatrices(int matrix1[][2], int matrix2[][2], int result[][2], int rows, 
     }
 }
 
+void subMatrices(int matrix1[][2], int matrix2[][2], int result[][2], int rows, int cols) {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            result[i][j] = matrix1[i][j] - matrix2[i][j];
+        }
+    }
+}
