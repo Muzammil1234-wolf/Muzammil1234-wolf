7ed3dff0e54942cba390f3466a1a2169e309b339
Muzammil1234-wolf <140607238+Muzammil1234-wolf@users.noreply.github.com>
2025-02-17 19:50:01 +0500

    Update Additionoperationonmatrices.cpp

--- Additionoperationonmatrices.cpp
+++ Additionoperationonmatrices.cpp
@@ -20,29 +20,3 @@ void addMatrices(int matrix1[][2], int matrix2[][2], int result[][2], int rows, 
     }
 }
 
-int main() {
-    int matrix1[2][2] = {
-        {1, 2},
-        {4, 5}
-    };
-    
-    int matrix2[2][2] = {
-        {5, 4},
-        {2, 1}
-    };
-    
-    int result[2][2];
-
-    cout << "Matrix 1:" << endl;
-    printMatrix(matrix1, 2, 2);
-    
-    cout << "Matrix 2:" << endl;
-    printMatrix(matrix2, 2, 2);
-
-    addMatrices(matrix1, matrix2, result, 2, 2);
-
-    cout << "Resultant Matrix (Addition):" << endl;
-    printMatrix(result, 2, 2);
-
-    return 0;
-}
